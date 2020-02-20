f18fc506824bce552681ea398229dc307786119d
Nolwenn Violette <lab661@PC-444.urel.feec.vutbr.cz>
2020-02-20 09:44:36 +0100

    [LAB] Adding 03-segment lab

---
+++ Labs/03-segment/hex_to_segment/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "/home/lab661/Documents/xpaule03/Digital-electronics-1/Labs/03-segment/hex_to_segment/top_isim_beh.exe" -prj "/home/lab661/Documents/xpaule03/Digital-electronics-1/Labs/03-segment/hex_to_segment/top_beh.prj" "work.top" 
