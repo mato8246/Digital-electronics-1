15b6f952f5cbb7030311e19a422267c3badcf69d
Nolwenn Violette <lab661@PC-444.urel.feec.vutbr.cz>
2020-03-05 10:01:13 +0100

    [LAB] Adding 05-counter_binary

---
+++ Labs/05-counter_binary/binary_counter/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "/home/lab661/Documents/xpaule03/Digital-electronics-1/Labs/05-counter_binary/binary_counter/binary_cnt_sb00_isim_beh.exe" -prj "/home/lab661/Documents/xpaule03/Digital-electronics-1/Labs/05-counter_binary/binary_counter/binary_cnt_sb00_beh.prj" "work.binary_cnt_sb00" 
